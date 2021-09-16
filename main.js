c99dcc5027a8fcff1bd9a636d333217129b8d780
vamanuilov <vamanuilov@gmail.com>
2021-09-17 01:12:59 +0700

    add slider

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import init from './fileController.js'
 import './submitController.js'
 import './dynamicProgress.js'
+import './slider.js'
 
 init()
---
+++ slider.js
@@ -0,0 +1,55 @@
+const slideIndexController = () => {
+  let slideIndex = 1
+  return {
+    getIndex: () => slideIndex,
+    plusIndex: () => {
+      slideIndex += 1
+    },
+    minusIndex: () => {
+      slideIndex -= 1
+    },
+    setIndex: (newSlideIndex) => {
+      slideIndex = newSlideIndex
+    },
+  }
+}
+
+const slideIndex = slideIndexController()
+const slides = document.getElementsByClassName('slider-image')
+const dots = document.querySelector('.dots-block').querySelectorAll('div')
+
+const showSlide = () => {
+  ;[...slides].forEach((slide, index) => {
+    if (slide.classList.contains('showen')) {
+      slide.classList.remove('showen')
+      dots[index].classList.remove('active-dot')
+    }
+    if (index + 1 === slideIndex.getIndex()) {
+      slide.classList.add('showen')
+      dots[index].classList.add('active-dot')
+    }
+  })
+}
+
+;[...dots].forEach((dot, index) => {
+  dot.addEventListener('click', () => {
+    slideIndex.setIndex(index + 1)
+    showSlide()
+  })
+})
+
+document.getElementById('right-slider-button').addEventListener('click', () => {
+  slideIndex.plusIndex()
+  if (slideIndex.getIndex() > slides.length) {
+    slideIndex.setIndex(1)
+  }
+  showSlide()
+})
+
+document.getElementById('left-slider-button').addEventListener('click', () => {
+  slideIndex.minusIndex()
+  if (slideIndex.getIndex() < 1) {
+    slideIndex.setIndex(slides.length)
+  }
+  showSlide()
+})
