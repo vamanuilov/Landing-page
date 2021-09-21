7099ed775c417969b3def8a78f86b5e35e7e9230
vamanuilov <vamanuilov@gmail.com>
2021-09-22 02:25:48 +0700

    add carousel

--- slider.js
+++ slider.js
@@ -1,5 +1,5 @@
 const slideIndexController = () => {
-  let slideIndex = 1
+  let slideIndex = 0
   return {
     getIndex: () => slideIndex,
     plusIndex: () => {
@@ -16,42 +16,71 @@ const slideIndexController = () => {
 
 const slideIndex = slideIndexController()
 const slides = document.getElementsByClassName('slider-image')
+const slideWrapper = document.querySelector('.slider-images-block')
 const dots = document.querySelector('.dots-block').querySelectorAll('div')
 
+const moveToFirstSlide = () => {
+  slideWrapper.classList.add('no-transition')
+  slideIndex.setIndex(0)
+  slideWrapper.style.setProperty('--slideIndex', slideIndex.getIndex())
+  return slideWrapper.removeEventListener('transitionend', moveToFirstSlide)
+}
+
+const moveToLastSlide = () => {
+  slideWrapper.classList.add('no-transition')
+  slideIndex.setIndex(slides.length - 1)
+  slideWrapper.style.setProperty('--slideIndex', slideIndex.getIndex())
+  setTimeout(() => {
+    slideWrapper.classList.remove('no-transition')
+    slideIndex.setIndex(slides.length - 1)
+    slideWrapper.style.setProperty('--slideIndex', slideIndex.getIndex())
+  }, 10)
+}
+
 const showSlide = () => {
-  ;[...slides].forEach((slide, index) => {
-    if (slide.classList.contains('showen')) {
-      slide.classList.remove('showen')
-      dots[index].classList.remove('active-dot')
-    }
-    if (index + 1 === slideIndex.getIndex()) {
-      slide.classList.add('showen')
-      dots[index].classList.add('active-dot')
-    }
-  })
+  // TODO: fix dots
+  // ;[...slides].forEach((slide, index) => {
+  //   if (slide.classList.contains('showen')) {
+  //     slide.classList.remove('showen')
+  //     dots[index].classList.remove('active-dot')
+  //   }
+  //   if (index === slideIndex.getIndex()) {
+  //     slide.classList.add('showen')
+  //     dots[index].classList.add('active-dot')
+  //   }
+  // })
 }
 
 export const addSlider = () => {
-  ;[...dots].forEach((dot, index) => {
-    dot.addEventListener('click', () => {
-      slideIndex.setIndex(index + 1)
-      showSlide()
-    })
-  })
+  slideWrapper.appendChild(slideWrapper.children[0].cloneNode(true))
+
+  // ;[...dots].forEach((dot, index) => {
+  //   dot.addEventListener('click', () => {
+  //     slideIndex.setIndex(index + 1)
+  //     showSlide()
+  //   })
+  // })
 
   document.getElementById('rightSliderButton').addEventListener('click', () => {
-    slideIndex.plusIndex()
-    if (slideIndex.getIndex() > slides.length) {
-      slideIndex.setIndex(1)
+    if (slideWrapper.classList.contains('no-transition')) {
+      slideWrapper.classList.remove('no-transition')
     }
+
+    slideIndex.plusIndex()
+    if (slideIndex.getIndex() >= slides.length - 1) {
+      slideWrapper.addEventListener('transitionend', moveToFirstSlide)
+    }
+    slideWrapper.style.setProperty('--slideIndex', slideIndex.getIndex())
     showSlide()
   })
 
   document.getElementById('leftSliderButton').addEventListener('click', () => {
     slideIndex.minusIndex()
-    if (slideIndex.getIndex() < 1) {
-      slideIndex.setIndex(slides.length)
+    if (slideIndex.getIndex() < 0) {
+      moveToLastSlide()
     }
+
+    slideWrapper.style.setProperty('--slideIndex', slideIndex.getIndex())
     showSlide()
   })
 }
