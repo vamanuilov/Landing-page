4e1cd5ddfc47e8ecb5c266a0b03cd7aee6c0b10a
vamanuilov <vamanuilov@gmail.com>
2021-09-23 02:01:11 +0700

    fix dots and small device screen carousel + some bug fixes

--- slider.js
+++ slider.js
@@ -17,12 +17,33 @@ const slideIndexController = () => {
 const slideIndex = slideIndexController()
 const slides = document.getElementsByClassName('slider-image')
 const slideWrapper = document.querySelector('.slider-images-block')
-const dots = document.querySelector('.dots-block').querySelectorAll('div')
+const dotsWrapper = document.querySelector('.dots-block')
+
+const setActiveDot = () => {
+  const dots = document.querySelector('.dots-block').querySelectorAll('div')
+
+  dots.forEach((dot, index) => {
+    if (dot.classList.contains('active-dot')) {
+      dot.classList.remove('active-dot')
+    }
+    if ((index === 0 && slideIndex.getIndex() === slides.length - 1) || index === slideIndex.getIndex()) {
+      dot.classList.add('active-dot')
+    }
+  })
+}
 
 const moveToFirstSlide = () => {
   slideWrapper.classList.add('no-transition')
   slideIndex.setIndex(0)
   slideWrapper.style.setProperty('--slideIndex', slideIndex.getIndex())
+
+  setTimeout(() => {
+    slideWrapper.classList.remove('no-transition')
+    slideIndex.plusIndex()
+    slideWrapper.style.setProperty('--slideIndex', slideIndex.getIndex())
+    setActiveDot()
+  }, 10)
+
   return slideWrapper.removeEventListener('transitionend', moveToFirstSlide)
 }
 
@@ -30,57 +51,56 @@ const moveToLastSlide = () => {
   slideWrapper.classList.add('no-transition')
   slideIndex.setIndex(slides.length - 1)
   slideWrapper.style.setProperty('--slideIndex', slideIndex.getIndex())
+
   setTimeout(() => {
     slideWrapper.classList.remove('no-transition')
-    slideIndex.setIndex(slides.length - 1)
+    slideIndex.setIndex(slides.length - 2)
     slideWrapper.style.setProperty('--slideIndex', slideIndex.getIndex())
+    setActiveDot()
   }, 10)
-}
-
-const showSlide = () => {
-  // TODO: fix dots
-  // ;[...slides].forEach((slide, index) => {
-  //   if (slide.classList.contains('showen')) {
-  //     slide.classList.remove('showen')
-  //     dots[index].classList.remove('active-dot')
-  //   }
-  //   if (index === slideIndex.getIndex()) {
-  //     slide.classList.add('showen')
-  //     dots[index].classList.add('active-dot')
-  //   }
-  // })
 }
 
 export const addSlider = () => {
   slideWrapper.appendChild(slideWrapper.children[0].cloneNode(true))
+  slideWrapper.style.setProperty('--slidesCount', slides.length)
+  dotsWrapper.style.width = `${(slides.length - 1) * 25}px`
 
-  // ;[...dots].forEach((dot, index) => {
-  //   dot.addEventListener('click', () => {
-  //     slideIndex.setIndex(index + 1)
-  //     showSlide()
-  //   })
-  // })
+  new Array(slides.length - 1)
+    .fill(null)
+    .map((el, index) => index)
+    .forEach((dotIndex) => {
+      const div = document.createElement('div')
+
+      div.addEventListener('click', () => {
+        slideIndex.setIndex(dotIndex)
+        slideWrapper.style.setProperty('--slideIndex', dotIndex)
+        setActiveDot()
+      })
+
+      if (dotIndex === slideIndex.getIndex()) {
+        div.classList.add('active-dot')
+      }
+
+      dotsWrapper.appendChild(div)
+    })
 
   document.getElementById('rightSliderButton').addEventListener('click', () => {
-    if (slideWrapper.classList.contains('no-transition')) {
-      slideWrapper.classList.remove('no-transition')
-    }
-
-    slideIndex.plusIndex()
-    if (slideIndex.getIndex() >= slides.length - 1) {
+    if (slideIndex.getIndex() === slides.length - 1) {
       slideWrapper.addEventListener('transitionend', moveToFirstSlide)
+    } else {
+      slideIndex.plusIndex()
+      slideWrapper.style.setProperty('--slideIndex', slideIndex.getIndex())
+      setActiveDot()
     }
-    slideWrapper.style.setProperty('--slideIndex', slideIndex.getIndex())
-    showSlide()
   })
 
   document.getElementById('leftSliderButton').addEventListener('click', () => {
     slideIndex.minusIndex()
     if (slideIndex.getIndex() < 0) {
       moveToLastSlide()
+    } else {
+      slideWrapper.style.setProperty('--slideIndex', slideIndex.getIndex())
+      setActiveDot()
     }
-
-    slideWrapper.style.setProperty('--slideIndex', slideIndex.getIndex())
-    showSlide()
   })
 }
