c9816c717ea07451001d72b41bf84d3c3900d216
vamanuilov <vamanuilov@gmail.com>
2021-09-23 22:59:19 +0700

    change date input to custom text

--- validation.js
+++ validation.js
@@ -4,8 +4,10 @@ import { file } from './fileController.js'
 const nameRegex = /^[a-zA-Zа-яА-Я-]{1,52}$/
 const textRegex = /^[a-zA-Zа-яА-Я-\s]{1,52}$/
 const dateRegex = /^(3[01]|[12][0-9]|0?[1-9])\.(1[012]|0?[1-9])\.((?:19|20)\d{2})*$/
+const numberRegex = /[^0-9.]/
 
 const formDate = document.getElementById('formDate')
+const textInputs = document.getElementById('userForm').querySelectorAll('input[type=text]')
 
 export const showErrorLabel = (elementId) => {
   document.getElementById(elementId).classList.add('error')
@@ -17,50 +19,55 @@ export const hideErrorLabel = (elementId) => {
   document.querySelector(`label[for="${elementId}"]`).classList.remove('label-error')
 }
 
+const isRegexForInputValid = (value, elementId) => {
+  switch (elementId) {
+    case 'formUsername':
+      return nameRegex.test(value)
+    case 'formDate':
+      return dateRegex.test(value)
+    default:
+      return textRegex.test(value)
+  }
+}
+
+const checkTextInput = (value, elementId) => {
+  if (!isNotEmpty(value) || !isRegexForInputValid(value, elementId)) {
+    showErrorLabel(elementId)
+    return false
+  }
+
+  return true
+}
+
 export const addValidation = () => {
-  formUsername.addEventListener('change', (inputElement) => {
-    if (!nameRegex.test(inputElement.target.value)) {
-      showErrorLabel(inputElement.target.id)
+  formUsername.addEventListener('input', (event) => {
+    if (event.target.classList.contains('error') && nameRegex.test(event.target.value)) {
+      hideErrorLabel(event.target.id)
     }
   })
 
-  formUsername.addEventListener('input', (inputElement) => {
-    if (inputElement.target.classList.contains('error') && nameRegex.test(inputElement.target.value)) {
-      hideErrorLabel(inputElement.target.id)
+  formDate.addEventListener('input', (event) => {
+    if (event.target.value.length === 2 && !event.inputType.includes('delete')) {
+      event.target.value += '.'
     }
+
+    if (event.target.value.length === 5 && !event.inputType.includes('delete')) {
+      event.target.value += '.'
+    }
+
+    event.target.value = event.target.value.replace(numberRegex, '')
   })
 
-  document
-    .querySelector('.form-additional-info')
-    .querySelectorAll('input[type=text]')
-    .forEach((el) => {
-      el.addEventListener('change', (inputElement) => {
-        if (!textRegex.test(inputElement.target.value)) {
-          showErrorLabel(inputElement.target.id)
-        }
-      })
-
-      el.addEventListener('input', (inputElement) => {
-        if (inputElement.target.classList.contains('error') && textRegex.test(inputElement.target.value)) {
-          hideErrorLabel(inputElement.target.id)
-        }
-      })
+  textInputs.forEach((el) => {
+    el.addEventListener('change', (event) => {
+      checkTextInput(event.target.value, event.target.id)
     })
 
-  formDate.addEventListener('blur', (inputElement) => {
-    const inputDate = new Date(inputElement.target.value).toLocaleDateString('ru-RU')
-
-    if (!dateRegex.test(inputDate)) {
-      showErrorLabel(inputElement.target.id)
-    }
-  })
-
-  formDate.addEventListener('input', (inputElement) => {
-    const inputDate = new Date(inputElement.target.value).toLocaleDateString('ru-RU')
-
-    if (inputElement.target.classList.contains('error') && dateRegex.test(inputDate)) {
-      hideErrorLabel(inputElement.target.id)
-    }
+    el.addEventListener('input', (event) => {
+      if (event.target.classList.contains('error') && isRegexForInputValid(event.target.value, event.target.id)) {
+        hideErrorLabel(event.target.id)
+      }
+    })
   })
 
   formSubmitButton.addEventListener('click', (event) => {
@@ -68,30 +75,14 @@ export const addValidation = () => {
     event.stopPropagation()
     const completeBlock = document.getElementById('completeBlockWrap')
 
-    const isTextValid = [...document.getElementById('userForm').querySelectorAll('input[type=text]')].reduce(
-      (acc, element) => {
-        const { value, id } = element
-        if (id === 'formUsername' && !isNotEmpty(value) && !nameRegex.test(value)) {
-          showErrorLabel(id)
-          return false
-        }
-        if (!isNotEmpty(value) && textRegex.test(value)) {
-          showErrorLabel(id)
-          return false
-        }
-        return acc && true
-      },
-      true
-    )
+    const isTextValid = [...textInputs].reduce((acc, element) => {
+      const { value, id } = element
 
-    const isDateValid =
-      isNotEmpty(formDate.value) && dateRegex.test(new Date(formDate.value).toLocaleDateString('ru-RU'))
+      return acc && checkTextInput(value, id)
+    }, true)
+
     const isSelectNotEmpty = isNotEmpty(formUserGender.value)
     const isFileNotEmpty = isNotEmpty(fileUpload.value) || file?.type.includes('image')
-
-    if (!isDateValid) {
-      showErrorLabel(formDate.id)
-    }
 
     if (!isSelectNotEmpty) {
       showErrorLabel(formUserGender.id)
@@ -101,7 +92,7 @@ export const addValidation = () => {
       showErrorLabel(fileUpload.id)
     }
 
-    if (isFileNotEmpty && isSelectNotEmpty && isDateValid && isTextValid) {
+    if (isFileNotEmpty && isSelectNotEmpty && isTextValid) {
       completeBlock.classList.remove('hidden')
       event.target.disabled = true
     }
